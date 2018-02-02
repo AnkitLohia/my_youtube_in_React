6406fe220468552e7ed51d5e625e34f8cc1679e0
Ankit Lohia <lohia@deshaw.com>
2018-02-02 17:44:10 +0530

    auto searching input search term

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ npm install
 npm start
 http://localhost:8080/webpack-dev-server/
 
-
+npm install --save lodash // librabry to throttle number of function calls
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
@@ -15,6 +16,10 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = { videos: [], selectedVideo: null };
+    this.videoSearch('surfboard');
+  }
+  
+  videoSearch(term) {
     YTSearch({key:API_KEY, searchKey: 'surfboard'}, (videos) => {
         this.setState({videos: videos, selectedVideo: videos[0]});
     });
@@ -23,11 +28,11 @@ class App extends Component {
   render () {
     return (
         <div>
-          <SearchBar />
+          <SearchBar onTermChange={term => this.videoSearch(term)} />
           <VideoDetail video={this.state.selectedVideo} />
           <VideoList 
-          videos={this.state.videos}
-          onVideoSelect = {selectedVideo => this.setState({selectedVideo}) } />
+            videos={this.state.videos}
+            onVideoSelect = {selectedVideo => this.setState({selectedVideo}) } />
         </div>
     );
   }
